56c0ec53b092c536f20f2c23243857e64c008ca2
DiegoHFA <diegourehor@gmail.com>
2015-11-16 14:05:56 +0100

    fusionado la rama desarrollo 	modificado: uno.java

--- uno.java
+++ uno.java
@@ -5,6 +5,8 @@ public class uno {
 	
 	public static void main (String args[]) {
 		System.out.println("estamos en una nueva fase de desarrollo");
+		System.out.println("añado esta nueva linea en master");
+
 	}
 }
 
